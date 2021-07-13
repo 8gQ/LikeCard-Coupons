fa24517a1e526233c93731f0158d4429a7dbe3d9
HMD <67750979+8gQ@users.noreply.github.com>
2021-07-12 19:26:18 -0700

    Update LikeCard.php

--- LikeCard.php
+++ LikeCard.php
@@ -2,17 +2,17 @@
 
 $request = file_get_contents("https://influencer.redeemly.com/influencer/5f7b125e8539127b2e2c617c");
 preg_match_all('/{"props":{"pageProps":{"code":"(.*?)"}/', $request, $code);
-echo " _____                         _     
-/ ____|                            ( )    
+echo "  _____                              _     
+ / ____|                            ( )    
 | |     ___  _   _ _ __   ___  _ __ |/ ___ 
 | |    / _ \| | | | '_ \ / _ \| '_ \  / __|
 | |____ (_) | |_| | |_) | (_) | | | | \__ \
  \_____\___/ \__,_| .__/ \___/|_| |_| |___/
                                   | |                      
                                   |_|                      
-                                            
+                                        
 [+] - This Tool For Free . Made By HMD @e31L
-[+] - Your Code : " . $code[1][0] . "\n";
+[+] - Your Code : " . $code[1][0];
 
 # telegram : https://t.me/RRLRR #
 ?>
