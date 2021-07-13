5cb6bf6a24df5678a923db0193561d50695acf02
HMD <67750979+8gQ@users.noreply.github.com>
2021-07-12 19:24:06 -0700

    Update LikeCard.php

--- LikeCard.php
+++ LikeCard.php
@@ -2,9 +2,9 @@
 
 $request = file_get_contents("https://influencer.redeemly.com/influencer/5f7b125e8539127b2e2c617c");
 preg_match_all('/{"props":{"pageProps":{"code":"(.*?)"}/', $request, $code);
-echo " _____  _         _____                              _     
- / ____|                            ( )    
- | |     ___  _   _ _ __   ___  _ __ |/ ___ 
+echo " _____                         _     
+/ ____|                            ( )    
+| |     ___  _   _ _ __   ___  _ __ |/ ___ 
 | |    / _ \| | | | '_ \ / _ \| '_ \  / __|
 | |____ (_) | |_| | |_) | (_) | | | | \__ \
  \_____\___/ \__,_| .__/ \___/|_| |_| |___/
