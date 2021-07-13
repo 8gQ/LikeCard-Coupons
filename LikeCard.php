5162c31914f80d3cd435b5c1bb61838e1a11dc5d
HMD <67750979+8gQ@users.noreply.github.com>
2021-07-12 19:11:25 -0700

    Update LikeCard.php

--- LikeCard.php
+++ LikeCard.php
@@ -3,17 +3,17 @@
 $request = file_get_contents("https://influencer.redeemly.com/influencer/5f7b125e8539127b2e2c617c");
 preg_match_all('/{"props":{"pageProps":{"code":"(.*?)"}/', $request, $code);
 echo " _____  _         _____                              _     
-|   \| |       / ____|                            ( )    
-| |) | |      | |     ___  _   _ _    ___  _  |/ ___ 
-|  _  /| |      | |    / _ \| | | | '_ \ / _ \| '_ \  / |
-| | \ \| |____  | |____ (_) | |_| | |_) | (_) | | | | \ \
+|  __ \| |       / ____|                            ( )    
+| |__) | |      | |     ___  _   _ _ __   ___  _ __ |/ ___ 
+|  _  /| |      | |    / _ \| | | | '_ \ / _ \| '_ \  / __|
+| | \ \| |____  | |____ (_) | |_| | |_) | (_) | | | | \__ \
 |_|  \_\______|  \_____\___/ \__,_| .__/ \___/|_| |_| |___/
                                   | |                      
                                   |_|                      
                                     
 [+] - This Tool For Free . Made By HMD @e31L \n";
 echo "[+] - Coder : @RRLRR , Channel : @CoderStock \n";
-echo "[+] - Your Code : " . print_r($code[1]) . "\n";
+echo "[+] - Your Code : " . $code[1] . "\n";
 
 # telegram : https://t.me/RRLRR #
 ?>
